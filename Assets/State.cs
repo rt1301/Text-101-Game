dc7e1012d3159b7283b449f16fa7cf0253328893
Rishabh Taparia <rishabh.taparia5@gmail.com>
2020-10-09 23:28:15 +0530

    Added starting state for the game

---
+++ Assets/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "State", menuName = "Text 101/State", order = 0)]
+public class State : ScriptableObject {
+   [TextArea(14,10)] [SerializeField] string storyText;
+  
+}
